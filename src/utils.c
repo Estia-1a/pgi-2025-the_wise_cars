4676bdd4e720a5530883f87d6db0647d72d34c7f
Nathan Dbps <nathan.debeaupuis@etu.estia.fr>
2025-06-13 15:53:39 +0200

    create get_pixel

--- src/utils.c
+++ src/utils.c
@@ -6,4 +6,12 @@
  * @brief Here, you have to define functions of the pixel struct : getPixel and setPixel.
  * 
  */
+pixelRGB* get_pixel(unsigned char* data, const unsigned int width, const unsigned int height, const unsigned int n, const unsigned int x, const unsigned int y) {
 
+    if (!data || x >= width || y >= height) {
+        return NULL;
+    }
+    
+    int index = (y * width + x) * n;
+    return (pixelRGB*)&data[index];
+}
