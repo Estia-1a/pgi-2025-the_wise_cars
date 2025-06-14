618e8703b14ffbc455a21c1b6dee878ab86098f6
Nathan Dbps <nathan.debeaupuis@etu.estia.fr>
2025-06-14 17:22:38 +0200

    add condition on features.h

--- src/features.h
+++ src/features.h
@@ -3,12 +3,12 @@
 
 
 void helloWorld();
-void dimension ();
-void first_pixel();
-void second_line();
-void print_pixel();
+void dimension (char * source_path);
+void first_pixel(char *source_path);
+void second_line(char *source_path);
+void print_pixel(char *filename, int x, int y );
 void tenth_pixel(char *source_path);
-void stat_report();
+void stat_report(char *source_path);
 void color_red(char*source_path);
 void color_green(char*source_path);
 void color_blue(char*source_path);
