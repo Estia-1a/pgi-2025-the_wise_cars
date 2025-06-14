405b21231a1c18ea70fc36e83f4802db41780aa2
DE FRITSCH Evrard <evrard.defritsch@etu.estia.fr>
2025-06-14 15:39:52 +0200

    Remise à 0 fonctionnel

--- src/features.h
+++ src/features.h
@@ -3,17 +3,6 @@
 
 
 void helloWorld();
-void dimension ();
-void first_pixel();
-void second_line();
-void print_pixel();
-void tenth_pixel(char *source_path);
-void stat_report();
-void color_red(char*source_path);
-void color_green(char*source_path);
-void color_blue(char*source_path);
-void invert(char*source_path);
-void color_desaturate(char*source_path);
-void color_gray_luminance(char*source_path);
+void dimension (char *source_path);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -33,6 +33,10 @@ int main(int argc, char **argv) {
     /* helloworld() function is defined in feature.h and implemented in feature.c */
     helloWorld();
   }
+  if ( strncmp( configuration.command, "dimension", 11 ) == 0 ) {
+    /* helloworld() function is defined in feature.h and implemented in feature.c */
+    dimension(configuration.filenames[0]);
+  }
   /*
    * TO COMPLETE
    */
